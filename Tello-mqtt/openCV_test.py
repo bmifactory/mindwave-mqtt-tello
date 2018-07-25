941c9e0c59a4f619e5c79d7eb158b89c3aaecea2
piconewton <kim@piconewton.com>
2018-07-26 04:05:26 +0900

    update

---
+++ Tello-mqtt/openCV_test.py
@@ -0,0 +1,39 @@
+import sys
+import traceback
+import tellopy
+import av
+import cv2.cv2 as cv2  # for avoidance of pylint error
+import numpy
+
+
+def main():
+    drone = tellopy.Tello()
+
+    try:
+        drone.connect()
+        drone.wait_for_connection(60.0)
+
+        container = av.open(drone.get_video_stream())
+        frame_count = 0
+        while True:
+            for frame in container.decode(video=0):
+                frame_count = frame_count + 1
+                # skip first 300 frames
+                if frame_count < 300:
+                    continue
+                print(frame_count)
+                image = cv2.cvtColor(numpy.array(frame.to_image()), cv2.COLOR_RGB2BGR)
+                cv2.imshow('Original', image)
+                #cv2.imshow('Canny', cv2.Canny(image, 100, 200))
+                cv2.waitKey(1)
+
+    except Exception as ex:
+        exc_type, exc_value, exc_traceback = sys.exc_info()
+        traceback.print_exception(exc_type, exc_value, exc_traceback)
+        print(ex)
+    finally:
+        drone.quit()
+        cv2.destroyAllWindows()
+
+if __name__ == '__main__':
+    main()
